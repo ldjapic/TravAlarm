d47722013fcab98940d79121382606db454212c2
Morisa Manzella <manzella.morisa@gmail.com>
2020-08-18 21:49:40 -0400

    Use expo template

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
-// @flow
-import boot from "./src/boot/index";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const app = boot();
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
 
-export default app;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
